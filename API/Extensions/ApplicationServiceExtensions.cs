77cce23e9b1616b397008eed2ffe7e93ea1bb6f6
manny <manuelmisa@gmail.com>
2023-02-23 23:45:48 -0500

    End of Section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions //when static no need to instantiate this class, just use it
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services
+        , IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>(); //Interface and its implementation class (where the actual method resides)
+
+            return services;
+        }
+    }
+}
